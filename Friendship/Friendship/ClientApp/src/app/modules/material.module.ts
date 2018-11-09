f7a44bb1239d62b7e094734f084d022f808ae546
Wdog-999 <wdog_999@yahoo.com>
2018-11-08 22:48:16 -0500

    Added buttons/form to nav bar

--- Friendship/Friendship/ClientApp/src/app/modules/material.module.ts
+++ Friendship/Friendship/ClientApp/src/app/modules/material.module.ts
@@ -1,9 +1,10 @@
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule],
 })
 export class MyOwnCustomMaterialModule { }
