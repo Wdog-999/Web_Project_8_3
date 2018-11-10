10869ab2d41a42e0fab0aa93fd601fbb360d1f89
Wdog-999 <wdog_999@yahoo.com>
2018-11-09 21:45:23 -0500

    Completed Navbar

--- Friendship/Friendship/ClientApp/src/app/modules/material.module.ts
+++ Friendship/Friendship/ClientApp/src/app/modules/material.module.ts
@@ -1,10 +1,11 @@
 import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
 })
 export class MyOwnCustomMaterialModule { }
