d698a80bae6d165ba42f9271bebd7f310cdea342
Wdog-999 <wdog_999@yahoo.com>
2018-12-16 16:36:40 -0500

    Added User DTOs

---
+++ Friendship/Friendship/Data/FullUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Friendship.Data
+{
+    internal class FullUserDTO
+    {
+        public string ID { get; set; }
+        public string UserName { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Email { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Photo { get; set; }
+    }
+}
